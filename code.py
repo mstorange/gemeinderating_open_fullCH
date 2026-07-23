67b7968a70527e71b8d2f1478dc7de4d6eee214f
mstorange <ma.stutz@losinger-marazzi.ch>
2026-07-23 15:41:11 +0200

    Update code.py

--- code.py
+++ code.py
@@ -351,7 +351,7 @@ if check_password():
         # warum auch immer sind hier auch deutsche, italienische, etc. Polygone drin haha, diese nehmen wir raus, sie haben die BFS-NR 0
         gemeinden2d = gemeinden2d[gemeinden2d['bfs_nummer']!=0].reset_index(drop=True)
         gemeinden2d = gemeinden2d[['bfs_nummer', 'name','einwohnerzahl', 'geometry']]
-        gemeinden2d = gemeinden2d.replace('Fétigny', 'Fétigny-Ménières')
+        gemeinden2d = gemeinden2d.replace('Fétigny-Ménières', 'Fétigny')
         ##st.write('Welche Spalten hat gemeinden2d?')
         #st.write(gemeinden2d.columns)
         #st.write('Welche Spalten hat fd?')
